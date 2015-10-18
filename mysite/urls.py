53e485315061d2ed9e4a89942b93b79cc2c58441
Alina <alinchay@gmail.com>
2015-10-18 15:42:41 +0300

    refactoring

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.media import media
 
 urlpatterns = [
     # Examples:
